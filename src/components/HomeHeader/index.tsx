de41ce16e8ab3695d6adc62cc5b6f80f9bbcd678
cd-cd-cd <1262877491@qq.com>
2022-12-12 21:03:28 +0800

    feat: header头像

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,8 +1,19 @@
+import { Popover } from 'antd'
 import React from 'react'
 import style from './index.module.scss'
 
 export default function HomeHeader () {
+  const content = (
+    <div className={style.avatarContent}>
+      <div>个人中心</div>
+      <div>退出</div>
+    </div>
+  )
   return (
-    <div className={style.header}>HomeHeader </div>
+    <div className={style.header}>
+      <Popover placement="bottomRight" content={content} trigger="hover">
+        <div className={style.avatarBox}></div>
+      </Popover>
+    </div>
   )
 }
